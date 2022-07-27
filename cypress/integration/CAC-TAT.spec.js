6d72560dadce38eec0dec07dd6cc48b7b32aebaf
Cibele <cibelermallmann@gmail.com>
2022-07-26 22:58:55 -0300

    class 10

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,24 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+
+    this.beforeEach(function(){
         cy.visit('./src/index.html') 
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')        
+    })
+
+    it.only('preenche os campos obrigatórios e envie o formulário', function() {
+        const longText = 'Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste.'
+
+        cy.get('#firstName').type('Cibele')
+        cy.get('#lastName').type('Mallmann')
+        cy.get('#email').type('cibele@mallmann.com')
+        cy.get('#open-text-area').type(longText, {delay: 0})
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
   })
