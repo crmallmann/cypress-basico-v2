9b750980a68a4087e4ceb4b5a9d60a57939c8cbc
Cibele <cibelermallmann@gmail.com>
2022-08-08 22:28:35 -0300

    Exercises completed

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -12,48 +12,40 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     it('preenche os campos obrigatórios e envie o formulário', function() {
         const longText = 'Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste. Teste.'
-
         cy.get('#firstName').type('Cibele')
         cy.get('#lastName').type('Mallmann')
         cy.get('#email').type('cibele@mallmann.com')
         cy.get('#open-text-area').type(longText, {delay: 0})
-        cy.get('button[type="submit"]').click()
-
+        cy.contains('button', 'Enviar').click()
         cy.get('.success').should('be.visible')
     })
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
-
         cy.get('#firstName').type('Cibele')
         cy.get('#lastName').type('Mallmann')
         cy.get('#email').type('cibele@mallmann,com')
         cy.get('#open-text-area').type('Teste.')
-        cy.get('button[type="submit"]').click()
-
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')    
     })
 
     it('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
-
         cy.get('#phone')
         .type('abcdefghij')
         .should('have.value','')
     })
 
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
-
         cy.get('#firstName').type('Cibele')
         cy.get('#lastName').type('Mallmann')
         cy.get('#email').type('cibele@mallmann.com')
         cy.get('#phone-checkbox').click()
         cy.get('#open-text-area').type('Teste.')
-        cy.get('button[type="submit"]').click()
-
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')    
     })
 
-    it.only('preenche e limpa os campos nomes, sobrenome, email e telefone', function() {
-
+    it('preenche e limpa os campos nomes, sobrenome, email e telefone', function() {
         cy.get('#firstName')
           .type('Cibele') 
           .should('have.value', 'Cibele')
@@ -74,5 +66,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           .should('have.value', '1234567890')
           .clear()
           .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+      cy.contains('button', 'Enviar').click()
+      cy.get('.error').should('be.visible')    
+    })
+
+    it('envia o formuário com sucesso usando um comando customizado', function(){
+      cy.fillMandatoryFieldsAndSubmit()
+      cy.get('.success').should('be.visible')
     })
   })
