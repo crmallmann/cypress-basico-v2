58372526fdc945548d6bc6a0450718c8c875b4ff
Cibele <cibelermallmann@gmail.com>
2022-08-10 21:16:26 -0300

    class 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -77,4 +77,28 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.fillMandatoryFieldsAndSubmit()
       cy.get('.success').should('be.visible')
     })
+
+    it('seleciona um produto (YouTube) por seu texto', function(){
+      cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube')
+    }) 
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function(){
+      cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+    }) 
+
+    it('seleciona um produto (Blog) por seu índice', function(){
+      cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria')
+    }) 
+
+    it.only('seleciona um produto (Blog) por seu índice', function(){
+      cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog')
+    }) 
   })
