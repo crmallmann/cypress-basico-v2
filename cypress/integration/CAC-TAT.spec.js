bfac69a463307cea3cd6503c4dae385ae3419a5d
Cibele <cibelermallmann@gmail.com>
2022-08-15 22:02:13 -0300

    class 22 and 23

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -80,25 +80,31 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     it('seleciona um produto (YouTube) por seu texto', function(){
       cy.get('#product')
-      .select('YouTube')
-      .should('have.value', 'youtube')
+        .select('YouTube')
+        .should('have.value', 'youtube')
     }) 
 
     it('seleciona um produto (Mentoria) por seu valor (value)', function(){
       cy.get('#product')
-      .select('mentoria')
-      .should('have.value', 'mentoria')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
     }) 
 
     it('seleciona um produto (Blog) por seu índice', function(){
       cy.get('#product')
-      .select('mentoria')
-      .should('have.value', 'mentoria')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
     }) 
 
-    it.only('seleciona um produto (Blog) por seu índice', function(){
+    it('seleciona um produto (Blog) por seu índice', function(){
       cy.get('#product')
-      .select(1)
-      .should('have.value', 'blog')
+        .select(1)
+        .should('have.value', 'blog')
+    }) 
+
+    it.only('marca o tipo de atendimento "Feedback"', function() {
+      cy.get('input[type="radio"][value="feedback"]')
+        .check()
+        .should('be.checked')
     }) 
   })
